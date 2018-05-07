f06a15175d5752b0f4f59b0f046ebe412b1c098a
sammanthp007 <sammanbikramthapa@gmail.com>
2018-05-07 01:05:45 -0400

    Update UI in PostTableNodeCell
    - Changed the text color contrast
    - Updated inset of nodes
    - Reordered nodes

--- DySi Open/Constants.swift
+++ DySi Open/Constants.swift
@@ -37,13 +37,16 @@ struct Constants {
     }
     
     struct CellLayout {
-        static let FontSize: CGFloat = 14
+        static let TitleFontSize: CGFloat = 18
+        static let BodyFontSize: CGFloat = 16
+        static let MetaDataFontSize: CGFloat = 14
         static let HeaderHeight: CGFloat = 50
         static let UserImageHeight: CGFloat = 30
         static let HorizontalBuffer: CGFloat = 10
         static let VerticalBuffer: CGFloat = 5
         static let InsetForAvatar = UIEdgeInsets(top: HorizontalBuffer, left: 0, bottom: HorizontalBuffer, right: HorizontalBuffer)
         static let InsetForHeader = UIEdgeInsets(top: 0, left: HorizontalBuffer, bottom: 0, right: HorizontalBuffer)
+        static let InsetForBody = UIEdgeInsets(top: 0, left: HorizontalBuffer, bottom: 0, right: HorizontalBuffer)
         static let InsetForFooter = UIEdgeInsets(top: VerticalBuffer, left: HorizontalBuffer, bottom: VerticalBuffer, right: HorizontalBuffer)
     }
 }
